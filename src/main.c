c62b60480658d08422da24e27cc6e9d4b5a7a117
Nasccped <pedro.main.tec@gmail.com>
2025-12-10 15:01:22 +0000

    chore: run format

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	if (argc < 2)
-		printf("Hello \x1b[92mWorld\x1b[0m!\n");
-	else {
-		printf("Hello,\x1b[92m");
-		for (int i = 1; i < argc; i++) printf(" %s", argv[i]);
-		printf("\x1b[0m!\n");
-	}
-	return 0;
+    if (argc < 2)
+        printf("Hello \x1b[92mWorld\x1b[0m!\n");
+    else {
+        printf("Hello,\x1b[92m");
+        for (int i = 1; i < argc; i++)
+            printf(" %s", argv[i]);
+        printf("\x1b[0m!\n");
+    }
+    return 0;
 }
