d931bc99f0a592f64ffc212a30ec6999ac53e4c1
Nasccped <pedro.main.tec@gmail.com>
2025-12-11 02:34:25 +0000

    chore: add string -> citerator conversion

---
+++ src/my_string.c
@@ -0,0 +1,32 @@
+#include "my_string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Fullfils a CIterator based on a given string. Fails if any param is
+ * null pointer. */
+void push_string_to_citerator(CIterator *citerator, char *str) {
+    if (!str || !citerator)
+        return;
+    citerator_clear(citerator);
+    size_t len = strlen(str);
+    char **queue = (char **)malloc(len * sizeof(char *));
+    if (!queue)
+        return;
+    citerator->queue_len = len;
+    for (size_t i = 0; i < len; i++)
+        queue[i] = &str[i];
+    citerator->root_pointer = (void **)queue;
+    citerator->current = citerator->root_pointer[0];
+}
+
+/* Create a new CIterator function from the self string */
+CIterator *new_citerator_from_string(char *self) {
+    if (!self)
+        return NULL;
+    CIterator *citer = citerator_new();
+    if (!citer)
+        return NULL;
+    push_string_to_citerator(citer, self);
+    return citer;
+}
---
+++ src/my_string.h
@@ -0,0 +1,9 @@
+#ifndef _MY_STRING_H_
+#define _MY_STRING_H_
+
+#include "citer.h"
+
+void push_string_to_citerator(CIterator *, char *);
+CIterator *new_citerator_from_string(char *);
+
+#endif
